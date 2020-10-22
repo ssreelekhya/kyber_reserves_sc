c154790dd6d952f5b7f2360ba1464a8800425057
ducquangkstn <ducquang.kstn@gmail.com>
2020-10-22 10:23:31 +0700

    update coverage.sh

--- coverage.sh
+++ coverage.sh
@@ -10,16 +10,16 @@ rm -r ./.tempCoverageArtifacts
 
 if [ "$VERSION" == 'sol4' ]; then
   echo "Running sol4 coverage..."
-  yarn buidler coverage --config ./buidlerConfigSol4.js --testfiles "" --solcoverjs ".solcover.js"
+  yarn buidler coverage --config ./buidlerConfigSol4.js --testfiles "test/sol4/" --solcoverjs ".solcover.js"
 elif [ "$VERSION" == 'sol5' ]; then
   yarn buidler compile --config buidlerConfigSol4.js
   cp -R ./artifacts/ ./.tempCoverageArtifacts
   echo "Running sol5 coverage..."
-  yarn buidler coverage --config ./buidlerConfigSol5.js --testfiles "" --solcoverjs ".solcover.js"
+  yarn buidler coverage --config ./buidlerConfigSol5.js --testfiles "test/sol5/" --solcoverjs ".solcover.js"
 else
   echo "Running sol6 coverage..."
   yarn buidler compile --config buidlerConfigSol5.js &&
   yarn buidler compile --config buidlerConfigSol4.js
   cp -R ./artifacts/ ./.tempCoverageArtifacts
-  yarn buidler coverage --config ./buidlerConfigSol6.js --testfiles "" --solcoverjs ".solcover.js"
+  yarn buidler coverage --config ./buidlerConfigSol6.js --testfiles "test/sol6/" --solcoverjs ".solcover.js"
 fi
